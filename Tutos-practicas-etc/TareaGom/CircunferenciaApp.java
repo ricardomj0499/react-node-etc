e3c35a778aa54462c6a179fb075dfb804de236a2
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2022-10-24 23:35:48 -0600

    reset repo

--- Tutos-practicas-etc/TareaGom/CircunferenciaApp.java
+++
@@ -1,9 +0,0 @@
-
-public class CircunferenciaApp{
-    public static void main(String[] args){
-        
-
-
-    }
-
-}
--- Tutos-practicas-etc/react-course/src/Product.js
+++
@@ -1,9 +0,0 @@
-function Product() {
-  return <h1>Producto</h1>;
-}
-
-export function Bye() {
-  return <hi>bye bye you'll</hi>;
-}
-
-export default Product;
